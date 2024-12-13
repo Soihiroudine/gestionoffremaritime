832f24835b75672e19a1091f25294584e218eedf
Soihiroudine <59316622+Soihiroudine@users.noreply.github.com>
2024-12-13 13:23:08 +0300

    Rearengement des fichiers du projet

--- serveur/routes/contact.js
+++ serveur/routes/contact.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/contact', (req, res, next) => {
-    res.render('index');
+    res.render('contact');
 });
 
 module.exports = router;
